fdd386e536c5797b9081e550d6b0347d4e22caab
Rakibulislam1 <rirakib03@gmail.com>
2025-03-18 02:48:30 +0600

    debounce Added

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -2,28 +2,34 @@ import { useContext, useState } from "react";
 import searchIcon from "../../assets/search.svg";
 import { LocationsContext } from "../../context";
 import { getLocationsByName } from "../../data/location-data";
+import { useDebounce } from "../../hooks";
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
 
   const { setSelectedLocation } = useContext(LocationsContext);
 
-  function handleSearch(e) {
-    e.preventDefault();
-    const fetchLocation = getLocationsByName(searchTerm);
+  const doSearch = useDebounce((term) => {
+    const fetchLocation = getLocationsByName(term);
     setSelectedLocation({ ...fetchLocation });
-    console.log(fetchLocation);
+  }, 500);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    doSearch(value);
   }
 
   return (
-    <form onSubmit={handleSearch}>
+    <form>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
           required=""
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
         />
         <button type="submit">
           <img src={searchIcon} />
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+const useDebounce = (callback, delay) => {
+  const timeoutIdRef = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+      }
+    };
+  }, []);
+
+  const debounceCallback = (...args) => {
+    if (timeoutIdRef.current) {
+      clearTimeout(timeoutIdRef.current);
+    }
+
+    timeoutIdRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debounceCallback;
+};
+
+export default useDebounce;
